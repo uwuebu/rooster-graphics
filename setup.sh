079689f63e4f325b015a22e3b4f1f558ad03615f
yuriizare <yurii.zaremba.kb.2021@lpnu.ua>
2025-03-21 16:28:09 +0200

    test

--- setup.sh
+++ setup.sh
@@ -32,19 +32,20 @@ DownloadExtract() {
     destination=$2
     zipPath="$TempDir/temp.zip"
     curl -L -o "$zipPath" "$url"
-    unzip -o "$zipPath" -d "$destination"
+    unzip -o "$zipPath" -d "$TempDir"
+    extractedDir=$(find "$TempDir" -mindepth 1 -maxdepth 1 -type d)
+    mv "$extractedDir" "$destination"
     rm -f "$zipPath"
 }
 
 # Install GLM
 echo "Installing GLM..."
-DownloadExtract "https://github.com/g-truc/glm/archive/refs/tags/1.0.0.zip" "$TempDir"
-mv "$TempDir/glm-*/glm" "$HeadersDir/glm"
+DownloadExtract "https://github.com/g-truc/glm/archive/refs/tags/1.0.0.zip" "$HeadersDir/glm"
 
 # Install GLFW and build it as a static library
 echo "Installing GLFW..."
-DownloadExtract "https://github.com/glfw/glfw/archive/refs/tags/3.4.zip" "$TempDir"
-GLFWSourceDir="$TempDir/glfw-3.4"
+DownloadExtract "https://github.com/glfw/glfw/archive/refs/tags/3.4.zip" "$TempDir/glfw"
+GLFWSourceDir="$TempDir/glfw"
 GLFWBuildDir="$BuildDir/glfw"
 mkdir -p "$GLFWBuildDir"
 
@@ -64,27 +65,22 @@ mv "$GLFWSourceDir/include/GLFW" "$HeadersDir/GLFW"
 
 # Install Assimp
 echo "Installing Assimp..."
-DownloadExtract "https://github.com/assimp/assimp/archive/refs/tags/v5.2.5.zip" "$TempDir"
-mv "$TempDir/assimp-*/include/assimp" "$HeadersDir/assimp"
+DownloadExtract "https://github.com/assimp/assimp/archive/refs/tags/v5.2.5.zip" "$HeadersDir/assimp"
 
 # Install ImGui with docking
 echo "Installing ImGui..."
-DownloadExtract "https://github.com/ocornut/imgui/archive/refs/heads/docking.zip" "$TempDir"
-mv "$TempDir/imgui-docking" "$HeadersDir/imgui"
+DownloadExtract "https://github.com/ocornut/imgui/archive/refs/heads/docking.zip" "$HeadersDir/imgui"
 
 # Install glad
 echo "Installing glad..."
-DownloadExtract "https://github.com/Dav1dde/glad/archive/refs/tags/v0.1.34.zip" "$TempDir"
-mv "$TempDir/glad-*/include/glad" "$HeadersDir/glad"
+DownloadExtract "https://github.com/Dav1dde/glad/archive/refs/tags/v0.1.34.zip" "$HeadersDir/glad"
 
 # Install Khronos headers
 echo "Installing Khronos headers..."
-DownloadExtract "https://github.com/KhronosGroup/OpenGL-Registry/archive/refs/heads/main.zip" "$TempDir"
-mv "$TempDir/OpenGL-Registry-main/api/GL" "$HeadersDir/KHR"
+DownloadExtract "https://github.com/KhronosGroup/OpenGL-Registry/archive/refs/heads/main.zip" "$HeadersDir/KHR"
 
 # Install stb_image
 echo "Installing stb_image..."
-DownloadExtract "https://github.com/nothings/stb/archive/refs/heads/master.zip" "$TempDir"
-mv "$TempDir/stb-master/stb_image.h" "$HeadersDir/stb_image.h"
+DownloadExtract "https://github.com/nothings/stb/archive/refs/heads/master.zip" "$HeadersDir/stb_image.h"
 
 echo "Installation complete! Headers are in $HeadersDir, and GLFW static library is in $LibDir."
